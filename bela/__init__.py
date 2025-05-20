b47d29b2b43ab1ff4846c95e4a4470e2fe5b62b4
matt <mhyatt@luc.edu>
2025-05-20 00:06:53 +0000

    avoid robot.cam.pose, support action_is_pad

--- bela/__init__.py
+++ bela/__init__.py
@@ -1,3 +1,5 @@
 from pathlib import Path
 
+from . import common  # noqa: F401
+
 ROOT = Path(__file__).resolve().parent.parent
--- bela/common/datasets/util.py
+++ bela/common/datasets/util.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+import logging
 from pathlib import Path
 
 import jax
@@ -6,11 +7,17 @@ from pytorch3d.transforms import matrix_to_rotation_6d
 import torch
 from tqdm import tqdm
 
+import bela
+
 
 @dataclass
 class DataStats:
-    path: Path
-    stats: dict[str, dict[str, torch.Tensor]] = field(default_factory=lambda: dict)
+    head: str
+    stats: dict[str, dict[str, torch.Tensor]] = field(default_factory=lambda: {})
+
+    @property
+    def path(self):
+        return Path(bela.ROOT) / f"stats_{self.head}.pt"
 
     def load(self):
         stats = torch.load(self.path) if self.path.exists() else {}
@@ -19,16 +26,23 @@ class DataStats:
     def save(self):
         torch.save(self.stats, self.path)
 
-    def compute(self, dataset, batchspec, h):
+    def compute(self, dataset, batchspec):
         if not self.stats:
-            self.stats = compute_stats(dataset, batchspec, h)
+            self.stats = compute_stats(dataset, batchspec, head=self.head)
             self.save()
 
+    def maybe_compute(self, dataset, batchspec):
+        if not self.stats:
+            self.load()
+        if not self.stats:
+            self.compute(dataset, batchspec)
+        return self.stats
 
-def postprocess(batch, batchspec, h, flat=True):
+
+def postprocess(batch, batchspec, head, flat=True):
     if "task" in batch:
         batch.pop("task")  # it is annoying to pprint
-    batch = {k.replace("observation.", f"observation.{h}."): v for k, v in batch.items()}
+    batch = {k.replace("observation.", f"observation.{head}."): v for k, v in batch.items()}
     batch = {k.replace(".state.", "."): v for k, v in batch.items()}
 
     # move shared features
@@ -36,7 +50,7 @@ def postprocess(batch, batchspec, h, flat=True):
     sharespec = {k: v for k, v in batchspec.items() if k.startswith("observation.shared")}
 
     def canshare(k):
-        sharekey = k.replace(f"observation.{h}.", "observation.shared.")
+        sharekey = k.replace(f"observation.{head}.", "observation.shared.")
         return sharekey in sharespec, sharekey
 
     newbatch = {}
@@ -50,8 +64,7 @@ def postprocess(batch, batchspec, h, flat=True):
     batch = newbatch
 
     # create shared features that don't exist
-    myfeat = "observation.shared.cam.pose"
-    if h == "human":
+    if head == "human":
         kp3d = batch["observation.human.kp3d"]
         bs, t, n, *_ = kp3d.shape
         palm = kp3d[:, :, 0]  # b,t,n,3 -> b,t,3
@@ -63,7 +76,7 @@ def postprocess(batch, batchspec, h, flat=True):
         rot = batch["observation.human.mano.global_orient"]
         rot = matrix_to_rotation_6d(rot.reshape(-1, 3, 3))
         rot = rot.reshape(bs, t, -1)
-        batch[myfeat] = torch.cat([palm, rot], dim=-1)
+        batch["observation.shared.cam.pose"] = torch.cat([palm, rot], dim=-1)
 
         # convert mano.hand_pose to rotation_6d
         manopose = batch["observation.human.mano.hand_pose"]  # b,t,m,3,3
@@ -72,11 +85,20 @@ def postprocess(batch, batchspec, h, flat=True):
         manopose = manopose.reshape(bs, t, -1) if flat else manopose
         batch["observation.human.mano.hand_pose"] = manopose
 
-    if h == "robot":
-        pass
+        # all the actions will have the same pad
+        batch["action_is_pad"] = batch["observation.human.kp3d_is_pad"]
 
-    # pprint(spec(batch))
-    batch = {k: v for k, v in batch.items() if k in batchspec}
+    if head == "robot":
+        bs, t, *_ = batch["observation.robot.joints"].shape
+
+        logging.warning("cam.pose is not implemented")
+        # zeros bs,t,9
+        batch["observation.shared.cam.pose"] = torch.zeros((bs, t, 9), device=batch["observation.robot.joints"].device)
+        # all the actions will have the same pad
+        batch["action_is_pad"] = batch["observation.robot.joints_is_pad"]
+
+    batch = {k: v for k, v in batch.items() if k in batchspec or k == "action_is_pad"}
+
     # design action
     # everything is a state variable if image is not in the name
     isstate = lambda x: "image" not in x
@@ -90,24 +112,26 @@ def postprocess(batch, batchspec, h, flat=True):
             actions[k] = q
     batch = batch | actions
 
-    batch["heads"] = [h, "shared"]
+    batch["heads"] = [head, "shared"]
     return batch
 
 
-def compute_stats(dataset, batchspec, h):
-    # Compute statistics for normalization
-    # TODO you will need separate stats for actions even though shared
-    stats = {}
-    data = []
-    samples = list(range(len(dataset)))[:10]
+def compute_stats(dataset, batchspec, head):
+    """Compute statistics for normalization
+    you need separate stats for actions even with shared heads
+    """
+
+    stats, data = {}, []
+    samples = list(range(len(dataset)))[:100]
     for i in tqdm(samples, total=len(samples), desc="Computing stats", leave=False):
         d = dataset[i]
         d.pop("task")
         d = jax.tree.map(lambda x: x.unsqueeze(0), d)
-        d = postprocess(d, batchspec, h="human")
+        d = postprocess(d, batchspec, head=head)
         d.pop("heads")
         data.append(d)
     data = jax.tree.map(lambda *x: torch.concatenate((x)), data[0], *data[1:])
+    data.pop("action_is_pad")  # doesnt need stats
 
     def make_stat(stack):
         return {
@@ -118,11 +142,10 @@ def compute_stats(dataset, batchspec, h):
             "count": stack.shape[0],
         }
 
-    stats = jax.tree.map(lambda x: make_stat(x), data)
-
     def take_act(k, v):
         y = k[0].key.startswith("action") and isinstance(v, torch.Tensor)
         return v[0] if y else v
 
+    stats = jax.tree.map(lambda x: make_stat(x), data)
     stats = jax.tree.map_with_path(take_act, stats)
     return stats
